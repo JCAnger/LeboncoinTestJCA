bcbb012fc8dee51005692fd56cf730749c25e890
JC <>
2021-04-15 18:16:41 +0200

    Update Tableview & TableViewCell

--- LeboncoinTest/LBCHttpManager.swift
+++ LeboncoinTest/LBCHttpManager.swift
@@ -13,4 +13,45 @@ class LBCHttpManager: NSObject {
     let lbcUrl = URL(string: "https://raw.githubusercontent.com/leboncoin/paperclip/master/listing.json")!
     let categoriesUrl = URL(string: "https://raw.githubusercontent.com/leboncoin/paperclip/master/categories.json")!
 
+    func getClassifedIdFromLbc(completion: @escaping (_ result: [[String: Any]]) -> Void) {
+        let task = session.dataTask(with: lbcUrl, completionHandler: { data, response, error in
+
+            do {
+                // make sure this JSON is in the format we expect
+                if let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [[String: Any]] {
+                    // try to read out a string array
+                    print(json[0])
+                    completion(json)
+                    
+                }
+            } catch let error as NSError {
+                print("Failed to load: \(error.localizedDescription)")
+            }
+            
+            })
+        task.resume()
+    }
+
+    
+    func getCategoriesWithSuccess(success : @escaping (_ result: [[String: Any]]) -> Void, failure : @escaping (_ error : NSError) -> Void) {
+        let task = session.dataTask(with: categoriesUrl, completionHandler: { data, response, error in
+
+            do {
+                // make sure this JSON is in the format we expect
+                if let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [[String: Any]] {
+                    // try to read out a string array
+                    print(json)
+                    success(json)
+                    
+                }
+            } catch let error as NSError {
+                print("Failed to load: \(error.localizedDescription)")
+                failure(error)
+            }
+            
+            })
+        task.resume()
+        
+       //
+    }
 }
