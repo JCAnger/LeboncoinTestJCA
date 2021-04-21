81b646217d67b64d2787c14fe526e47dbb5c4fbe
JCA <>
2021-04-21 08:31:01 +0200

    Bugfix constraints

--- LeboncoinTest/LBCDetailViewController.swift
+++ LeboncoinTest/LBCDetailViewController.swift
@@ -57,6 +57,7 @@ class LBCDetailViewController: UIViewController {
         price.font = UIFont.boldSystemFont(ofSize: 20)
         price.textAlignment = .right
         date.font = UIFont.systemFont(ofSize: 16)
+        date.textAlignment = .right
         theDescription.font = UIFont.systemFont(ofSize: 16)
         
         isUrgent.frame.size = CGSize(width: 20, height: 20)
@@ -100,6 +101,7 @@ class LBCDetailViewController: UIViewController {
         dateFormatter.dateFormat = "dd/MM/yyyy"// yyyy-MM-dd'T'HH:mm:ss "
         self.date.text = dateFormatter.string(from: theSmallAd.creationDate)
         
+        
         self.picture.layer.borderWidth = 0.5
         self.view.backgroundColor = .white
         price.textColor = .orange
@@ -113,11 +115,9 @@ class LBCDetailViewController: UIViewController {
             self.isUrgent.backgroundColor = .clear
         }
         
-        topPictH = picture.topAnchor.constraint(equalTo: view.topAnchor, constant:  50)
-        topPictV = picture.topAnchor.constraint(equalTo: view.topAnchor, constant:  110)
-        
         picture.translatesAutoresizingMaskIntoConstraints = false
-        picture.topAnchor.constraint(equalTo: view.topAnchor, constant:  100).isActive = true // ok
+        topPictH = picture.topAnchor.constraint(equalTo: view.topAnchor, constant:  50)
+        topPictV = picture.topAnchor.constraint(equalTo: view.topAnchor, constant:  110) //***
         picture.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10 as CGFloat).isActive = true //ok
         heightPictV = picture.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.4)// ok
         if UIDevice.current.userInterfaceIdiom == .pad {
@@ -126,17 +126,14 @@ class LBCDetailViewController: UIViewController {
         else {
             heightPictH = picture.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.8)// ok
         }
-        widthPictV = picture.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.95) //
-        widthPictH = picture.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.4) //
+        widthPictH = picture.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.4) // ***
         
         contentView.translatesAutoresizingMaskIntoConstraints =  false
         topDescV = contentView.topAnchor.constraint(equalTo: picture.bottomAnchor, constant:  10) // TODO
-        topDescV.isActive = true
         topDescH = contentView.topAnchor.constraint(equalTo: view.topAnchor, constant:  50) // TODO
         leftDescV = contentView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10 as CGFloat) // TODO
-        leftDescV.isActive = true
         leftDescH = contentView.leftAnchor.constraint(equalTo: picture.rightAnchor, constant: 10 as CGFloat) // TODO
-        contentView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true // ok
+        contentView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -15).isActive = true // ok
         
         theTitle.translatesAutoresizingMaskIntoConstraints = false
         theTitle.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10 as CGFloat).isActive = true
@@ -162,7 +159,7 @@ class LBCDetailViewController: UIViewController {
         date.topAnchor.constraint(equalTo: theDescription.bottomAnchor, constant: 20 as CGFloat).isActive = true
         date.leftAnchor.constraint(equalTo: contentView.rightAnchor, constant: -150 as CGFloat).isActive = true
         date.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -20 as CGFloat).isActive = true
-        date.textAlignment = .right
+
         
         isUrgent.translatesAutoresizingMaskIntoConstraints = false
         isUrgent.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0 as CGFloat).isActive = true
@@ -190,13 +187,11 @@ class LBCDetailViewController: UIViewController {
         else { // portrait
             self.updateToPortait()
         }
-        
     }
     
     func updateToLandscape() {
-        topPictH.isActive = true
         topPictV.isActive = false
-        widthPictV.isActive = false
+        topPictH.isActive = true
         widthPictH.isActive = true
         heightPictV.isActive = false
         heightPictH.isActive = true
@@ -209,13 +204,13 @@ class LBCDetailViewController: UIViewController {
     func updateToPortait() {
         topPictH.isActive = false
         topPictV.isActive = true
-        widthPictV.isActive = true
         widthPictH.isActive = false
-        heightPictV.isActive = true
         heightPictH.isActive = false
-        topDescV.isActive = true
+        heightPictV.isActive = true
         topDescH.isActive = false
-        leftDescV.isActive = true
+        topDescV.isActive = true
         leftDescH.isActive = false
+        leftDescV.isActive = true
+        
     }
 }
